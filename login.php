5fe600c6f271021291c601ebcd26b59b332d7a74
Mec1527 <lucianomeccariello2004@gmail.com>
2023-05-15 18:05:20 +0200

    .

--- login.php
+++ login.php
@@ -1,49 +1,38 @@
 <?php
 session_start();
 include "connessione.php";
-         $data = new PDO("mysql:host=$servername; dbname=$databasename", $username,);  
-         $data->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
-         if(isset($_POST["login"]))  
-         {  
-              if(empty($_POST["email"]) || empty($_POST["password"]))  
-              {  
-                  echo 'spazio vuoto';  
-              }  
-              else  
-              {  
-               $statement = $data->prepare($query);
-                   $query = "SELECT id, email , password FROM utenti WHERE email = :email AND password = :password";
-                   $statement = $data->prepare($query);  
-                   $statement->execute(  
-                        array(  
-                             'email'     =>     $_POST["email"],  
-                             'password'     =>     $_POST["password"] ,
-                             
-                        )  
-                   );  
-              }  
-          
+$data = new PDO("mysql:host=$servername; dbname=$databasename", $username, );
+$data->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+if (isset($_POST["login"])) {
+     if (empty($_POST["email"]) || empty($_POST["password"])) {
+          echo 'spazio vuoto';
+     } else {
+          $statement = $data->prepare($query);
+          $query = "SELECT id, email , password FROM utenti WHERE email = :email AND password = :password";
+          $statement = $data->prepare($query);
+          $statement->execute(
+               array(
+                    'email' => $_POST["email"],
+                    'password' => $_POST["password"],
 
+               )
+          );
 
-                   
-                   foreach ($statement as $chiave)
-                   
-                   if($chiave>0)  
-                  
-                   {  
-                        $_SESSION["marco"] = $chiave['id'];  
+          $result = $statement->fetchAll();
 
-                        header("location:tabella.php");  
-                   }  
-                   else  
-                   {  
-                    header("location:login.html"); 
-                   }
-               
-         }  
+          if(count($result) > 0){
+               $_SESSION["marco"] = $result[0]['id'];
+               header("location:tabella.php");
+          }else{
+               echo "errore";
+               exit();
+          }
+     }
+
+}
+?>
            
   
 
      
-    ?>  
    
