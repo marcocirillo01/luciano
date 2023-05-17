d258b8fe0d350240fdf978e3634365b9edfcef3a
Mec1527 <lucianomeccariello2004@gmail.com>
2023-05-17 17:25:43 +0200

    ,

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 include "connessione.php";
-$data = new PDO("mysql:host=$servername; dbname=$databasename", $username, );
+$data = new PDO("mysql:host=$servername; dbname=$databasename", $username,);
 $data->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$pass= $_POST['password'];
-$email= $_POST['email'];
+$pass = $_POST['password'];
+$email = $_POST['email'];
 if (isset($_POST["login"])) {
      if (empty($_POST["email"]) || empty($_POST["password"])) {
           echo 'spazio vuoto';
@@ -17,31 +17,24 @@ if (isset($_POST["login"])) {
                     'password' => $_POST["password"],
                )
           );
-$result = $statement->fetchAll();
+          $result = $statement->fetchAll();
 
-$user=$data->prepare("SELECT password FROM utenti WHERE email = :email ");
-$user->bindParam(':email', $email);
-$user->execute();
-$row = $user->fetch(PDO::FETCH_ASSOC);
+          $user = $data->prepare("SELECT password FROM utenti WHERE email = :email ");
+          $user->bindParam(':email', $email);
+          $user->execute();
+          $row = $user->fetch(PDO::FETCH_ASSOC);
 
-$verifica=password_verify($pass,$row['password']);
-echo $verifica;
-if($verifica){
-          if(count($result) > 0){
-               $_SESSION["marco"] = $result[0]['id'];
-               header("location:tabella.php");
-          }else{
-               //header("location:login.html");
-               echo "errore";
-               exit();
+          $verifica = password_verify($pass, $row['password']);
+          echo $verifica;
+          if ($verifica) {
+               if (count($result) > 0) {
+                    $_SESSION["marco"] = $result[0]['id'];
+                    header("location:tabella.php");
+               } else {
+                    //header("location:login.html");
+                    echo "errore";
+                    exit();
+               }
           }
      }
-     }
-
 }
-?>
-   
-  
-
-     
-   
